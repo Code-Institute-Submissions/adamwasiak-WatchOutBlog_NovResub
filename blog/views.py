d53dd758aa8e6adb24e547214bb0503d4be86464
adamwasiak <adam.wasiak82@gmail.com>
2022-05-09 18:26:58 +0000

    Create comment structure

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View 
+from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
 
@@ -26,6 +27,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )

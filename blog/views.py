5b6de9ba79781d6be9617914158555f77086c56f
adamwasiak <adam.wasiak82@gmail.com>
2022-05-13 14:00:00 +0000

    Correct like/unlike function and add messages structure

--- blog/views.py
+++ blog/views.py
@@ -65,11 +65,11 @@ class PostDetail(View):
             },
         )
 
-
 class PostLike(View):
 
     def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
+        
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:

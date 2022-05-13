f4e961442796a479078f07a88c0e86ba92cc17e8
adamwasiak <adam.wasiak82@gmail.com>
2022-05-13 16:22:24 +0000

    Remove email field from model

--- blog/views.py
+++ blog/views.py
@@ -69,7 +69,7 @@ class PostLike(View):
 
     def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
-        
+
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
